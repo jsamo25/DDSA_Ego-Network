8b9010a0aaae157072a51ed1ed8cfbda69ffc823
Jorge <jsamo25@gmail.com>
2020-03-14 19:42:50 +0100

    Adding script to rewire nodes and calculate Assortativity.

--- DDSA_Ego_rewiring.R
+++ DDSA_Ego_rewiring.R
@@ -1,45 +1,40 @@
----
-title: "Mastodon Network Analysis"
-output: html_notebook
----
-
-
-```{r}
-install.packages("igraph")
-```
-
-```{r}
-mastodon_data <- read.csv("C:/Users/JORGE/Documents/MIIS_UPF/Data-Driven Social Analysis/Mini-project/mastodon_growth_from_1_16_to_3_16_anonim.csv",header=FALSE)
-
-```
-
-```{r}
 library(igraph)
+
+mastodon_data <- read.csv("C:/Users/JORGE/Documents/MIIS_UPF/Data-Driven Social Analysis/Mini-project/mastodon_growth_from_1_16_to_3_16_anonim.csv",header=FALSE)
 
 #Directed graph
 mastodon_digraph <- graph.data.frame(mastodon_data,directed=TRUE)
 
 #Undirected
 mastodon_ungraph <- graph.data.frame(mastodon_data,directed=FALSE)
-```
 
-```{r}
 #Find the node with highest degree
 V(mastodon_digraph)$name[degree(mastodon_digraph)==max(degree(mastodon_digraph))]
-```
 
-```{r}
 #Calculate the Assorsativity  https://igraph.org/r/doc/assortativity.html
 assortativity_degree(mastodon_digraph,directed=TRUE)
-```
 
-```{r}
 #direct neighbors
 neighbors(mastodon_digraph, v=which(V(mastodon_digraph)$name=="73@pawoo.net"))
-```
 
-```{r}
 #n-neighbors away
 ego(mastodon_digraph, order=2, nodes=which(V(mastodon_digraph)$name=="73@pawoo.net"))
-```
+
+
+# mean distance of the original graph
+mean_distance(mastodon_digraph)
+
+#node rewiring 0.05
+mastodon_digraph_rewired <- rewire(mastodon_digraph, each_edge(prob = 0.05))
+mean_distance(mastodon_digraph_rewired)
+
+#Assortativity after rewiring 0.05
+assortativity_degree(mastodon_digraph_rewired,directed=TRUE)
+
+#node rewiring 0.5
+mastodon_digraph_rewired_2 <- rewire(mastodon_digraph, each_edge(prob = 0.5))
+
+#Assortativity after rewiring 0.5
+assortativity_degree(mastodon_digraph_rewired_2,directed=TRUE)
+mean_distance(mastodon_digraph_rewired_2)
 
